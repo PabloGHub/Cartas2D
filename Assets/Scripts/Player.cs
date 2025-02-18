debe9f0ad6ff8bd737f4e4ced85fe61fb0fea76a
Sui <149364828+PabloGHub@users.noreply.github.com>
2025-02-18 12:34:20 +0100

    optimizado coger objeto

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,7 +10,7 @@ public class Player : MonoBehaviour
     // VARs //
     bool _levantando = false;
     GameObject _objetoRayCast;
-    FixedJoint2D _joint;
+    FixedJoint2D _eje;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -42,9 +42,9 @@ public class Player : MonoBehaviour
             if (_levantando == true)
             {
                 _levantando = false;
-                if (_joint != null)
+                if (_eje != null)
                 {
-                    Destroy(_joint);
+                    Destroy(_eje);
                 }
 
                 _objetoRayCast.transform.SetParent(null);
@@ -60,37 +60,37 @@ public class Player : MonoBehaviour
             }
             else
             {
-
-                // Lanzar un rayo hacia adelante desde la posición del jugador
                 RaycastHit2D _hit1 = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y + 1), _paLante_v2, 100, ~layerMask);
-                RaycastHit2D _hit2 = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y + 0.5f), _paLante_v2, 100, ~layerMask);
-                RaycastHit2D _hit3 = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - 0.5f), _paLante_v2, 100, ~layerMask);
-                RaycastHit2D _hit4 = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - 1), _paLante_v2, 100, ~layerMask);
-
-                // Verificar si el rayo ha colisionado con algún objeto
                 if (_hit1.collider != null)
                 {
                     Debug.Log("El rayo1 ha colisionado con: " + _hit1.collider.name);
                     cogerObjeto(_hit1.collider.gameObject);
                     _objetoRayCast = _hit1.collider.gameObject;
+                    return;
                 }
+                RaycastHit2D _hit2 = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y + 0.5f), _paLante_v2, 100, ~layerMask);
                 else if (_hit2.collider != null)
                 {
                     Debug.Log("El rayo2 ha colisionado con: " + _hit2.collider.name);
                     cogerObjeto(_hit2.collider.gameObject);
                     _objetoRayCast = _hit2.collider.gameObject;
+                    return;
                 }
+                RaycastHit2D _hit3 = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - 0.5f), _paLante_v2, 100, ~layerMask);
                 else if (_hit3.collider != null)
                 {
                     Debug.Log("El rayo3 ha colisionado con: " + _hit3.collider.name);
                     cogerObjeto(_hit3.collider.gameObject);
                     _objetoRayCast = _hit3.collider.gameObject;
+                    return;
                 }
+                RaycastHit2D _hit4 = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - 1), _paLante_v2, 100, ~layerMask);
                 else if (_hit4.collider != null)
                 {
                     Debug.Log("El rayo4 ha colisionado con: " + _hit4.collider.name);
                     cogerObjeto(_hit4.collider.gameObject);
                     _objetoRayCast = _hit4.collider.gameObject;
+                    return;
                 }
             }
         }
@@ -103,10 +103,10 @@ public class Player : MonoBehaviour
         if (_rbObjeto_rb != null)
         {
             _rbObjeto_rb.Sleep();
-            _joint = gameObject.AddComponent<FixedJoint2D>();
-            _joint.connectedAnchor = new Vector2(0, -1.6f);
-            _joint.connectedBody = _rbObjeto_rb;
-            _joint.autoConfigureConnectedAnchor = false;
+            _eje = gameObject.AddComponent<FixedJoint2D>();
+            _eje.connectedAnchor = new Vector2(0, -1.6f);
+            _eje.connectedBody = _rbObjeto_rb;
+            _eje.autoConfigureConnectedAnchor = false;
         }
 
         _Objeto_go.transform.SetParent(transform);
