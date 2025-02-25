9518072fb5881073480eef9a14b5f6417a3e664b
Sui <149364828+PabloGHub@users.noreply.github.com>
2025-02-25 09:21:44 +0100

    Carta.cs + Text mesh pro

---
+++ Assets/Scripts/Carta.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Carta : MonoBehaviour
+{
+    // **** Variables **** //
+    [SerializeField]
+    private int daño = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
