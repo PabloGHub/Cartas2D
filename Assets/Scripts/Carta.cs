2b7ecb6f5308b0b734e22bdbf136aae04eb8a7e3
Sui <149364828+PabloGHub@users.noreply.github.com>
2025-02-25 10:19:48 +0100

    Carta tiene niveles + Partida.unity

--- Assets/Scripts/Carta.cs
+++ Assets/Scripts/Carta.cs
@@ -1,15 +1,28 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Carta : MonoBehaviour
 {
     // **** Variables **** //
+    public int nivel = 1;
+
+    // Privados visibles.
     [SerializeField]
-    private int daño = 0;
+    private float daño = 0f;
+    [SerializeField]
+    private TextMeshProUGUI dañoReferencia;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        daño = Random.Range(2, 10) + (nivel * 1.25f);
+        //Debug.Log("Daño sumar: " + (nivel * 1.25f));
+
+        if (dañoReferencia != null)
+        {
+            dañoReferencia.text = daño.ToString();
+        }
     }
 
     // Update is called once per frame
