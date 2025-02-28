8bcf707970cf5129c9a4c137a3434aec37af299f
PabloGHub <149364828+PabloGHub@users.noreply.github.com>
2025-02-28 20:37:39 +0100

    Pilares de Cartas creado

--- Assets/Scripts/Puestos/Pilar.cs
+++ Assets/Scripts/Puestos/Pilar.cs
@@ -7,7 +7,7 @@ using System.Collections;
 public class Pilar : Absorber
 {
     // ****************** Metodos Unity ****************** //
-    public LayerMask _MascaraObjetos_lm;
+    public bool _cartaEncima_b = false;
 
     // ****************** Metodos Unity ****************** //
     void Start()
@@ -17,16 +17,19 @@ public class Pilar : Absorber
 
     void Update()
     {
-        RaycastHit2D _HitCarta_h = Physics2D.Raycast(transform.position - new Vector3(0f, -0.5f), Vector2.up, 0.5f, _MascaraObjetos_lm);
-        
-        if (_HitCarta_h)
+        if (transform.childCount <= 0)
         {
-            Debug.Log(_HitCarta_h.collider.name);
+            GetComponent<CircleCollider2D>().enabled = true;
             GetComponent<BoxCollider2D>().enabled = false;
+            _cartaEncima_b = false;
         }
         else
         {
+            GetComponent<CircleCollider2D>().enabled = false;
             GetComponent<BoxCollider2D>().enabled = true;
+            _cartaEncima_b = true;
+
+            moverEncima();
         }
     }
 
@@ -36,18 +39,27 @@ public class Pilar : Absorber
         //StartCoroutine(AnimacionSecuencial());
         _animator.Play("Aparecer");
 
-        Rigidbody2D _rbObjeto_rb = _objeto_go.GetComponent<Rigidbody2D>();
-        _rbObjeto_rb.Sleep();
-        _rbObjeto_rb.simulated = false;
-        
+        if (_objeto_go != null)
+        {
+            _objeto_go.GetComponent<Rigidbody2D>().simulated = false;
+
+            moverEncima();
+        }
+    }
+
+    void moverEncima()
+    {
         float alturaObjeto = gameObject.GetComponent<Renderer>().bounds.size.y;
 
         _objeto_go.transform.SetParent(transform);
         _objeto_go.transform.rotation = Quaternion.identity;
         _objeto_go.transform.localPosition = new Vector3(0f, (alturaObjeto / 2) + 0.8f, 0f);
 
+        _objeto_go.GetComponent<Rigidbody2D>().simulated = true;
     }
 
+    // Desvanecer esta descartado, automaticamente al ejecutar Aparecer pasa a Quieto.
+    // Desvanecer -> Aparecer -> Quieto
     private IEnumerator AnimacionSecuencial()
     {
         yield return StartCoroutine(EsperarAnimacion("Desvanecer", "Aparecer"));
