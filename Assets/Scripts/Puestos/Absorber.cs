2f3340b2b8b4ba2b42c148e5cb070622b7d19b17
PabloGHub <149364828+PabloGHub@users.noreply.github.com>
2025-02-28 14:39:26 +0100

    absorber debe funcionar

--- Assets/Scripts/Puestos/Absorber.cs
+++ Assets/Scripts/Puestos/Absorber.cs
@@ -1,19 +1,45 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+using System.Collections.Generic;
+using System.Collections;
 
 public class Absorber : MonoBehaviour
 {
+    // ****************** DECALRACIONES ****************** //
     protected GameObject _objeto_go;
     protected Animator _animator;
 
+
+    // ****************** Metodos UNITY ****************** //
     private void OnTriggerEnter2D(Collider2D _colision)
     {
         if (_colision.gameObject.CompareTag("Objeto"))
         {
             _animator = _colision.gameObject.GetComponentInChildren<Animator>();
             _animator.Play("Desvanecer");
-
             _objeto_go = _colision.gameObject;
-            new WaitForSeconds(0.5f);
+
+            //trasAbsorber();
         }
+    }
+
+
+    // ****************** Metodos NUESTROS ****************** //
+    protected IEnumerator animarRetardado(string _animacion_s, float _tiempo_f)
+    {
+        yield return new WaitForSeconds(_tiempo_f);
+        _animator.Play(_animacion_s);
+    } // StartCoroutine(animarRetardado("Aparecer"));
+
+
+    protected IEnumerator EsperarAnimacion(string _animacionActual_s, string _AnimacionSiguiente_s)
+    {
+        while (_animator.GetCurrentAnimatorStateInfo(0).IsName(_animacionActual_s) &&
+            _animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f)
+        {
+            yield return null;
+        }
+        _animator.Play(_AnimacionSiguiente_s);
     }
 }
--- Assets/Scripts/Puestos/Pilar.cs
+++ Assets/Scripts/Puestos/Pilar.cs
@@ -4,13 +4,8 @@ using static UnityEngine.RuleTile.TilingRuleOutput;
 using System.Collections.Generic;
 using System.Collections;
 
-public class Pilar : MonoBehaviour
+public class Pilar : Absorber
 {
-    // ****************** Variables ****************** //
-    protected GameObject _objeto_go;
-    protected Animator _animator;
-
-
     // ****************** Metodos Unity ****************** //
     void Start()
     {
@@ -22,38 +17,24 @@ public class Pilar : MonoBehaviour
 
     }
 
-    private void OnTriggerEnter2D(Collider2D _colision)
+    protected void trasAbsorber()
     {
-        if (_colision.gameObject.CompareTag("Objeto"))
+        EsperarAnimacion("Desvanecer", "Aparecer");
+        EsperarAnimacion("Aparecer", "Quieto");
+
+        if (_objeto_go != null) 
         {
-            _animator = _colision.gameObject.GetComponentInChildren<Animator>();
-            _animator.Play("Desvanecer");
-
-            _objeto_go = _colision.gameObject;
-
-            StartCoroutine(animarRetardado("Aparecer"));
-
             Rigidbody2D _rbObjeto_rb = _objeto_go.GetComponent<Rigidbody2D>();
-            if (_rbObjeto_rb != null)
-            {
-                _rbObjeto_rb.Sleep();
-                _rbObjeto_rb.simulated = false;
-            }
-            float alturaObjeto = gameObject.GetComponent<Renderer>().bounds.size.y;
-
-            _objeto_go.transform.SetParent(transform);
-            _objeto_go.transform.rotation = Quaternion.identity;
-            _objeto_go.transform.localPosition = new Vector3(0, (alturaObjeto / 2) + 1f, 0);
-
-            _objeto_go = null;
-
-            StartCoroutine(animarRetardado("Quieto"));
+            _rbObjeto_rb.Sleep();
+            _rbObjeto_rb.simulated = false;
         }
+        float alturaObjeto = gameObject.GetComponent<Renderer>().bounds.size.y;
+
+        _objeto_go.transform.SetParent(transform);
+        _objeto_go.transform.rotation = Quaternion.identity;
+        _objeto_go.transform.localPosition = new Vector3(0, (alturaObjeto / 2) + 1f, 0);
+
+        _objeto_go = null;
     }
 
-    IEnumerator animarRetardado(string _animacion_s)
-    {
-        yield return new WaitForSeconds(0.3f);
-        _animator.Play(_animacion_s);
-    }
 }
