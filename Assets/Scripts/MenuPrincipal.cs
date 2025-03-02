8555a9f3b9af48da9d696bcb91d6df44e08cb2a9
PabloGHub <149364828+PabloGHub@users.noreply.github.com>
2025-03-02 22:04:33 +0100

    MenuPrincipal bugs solucionado

--- Assets/Scripts/MenuPrincipal.cs
+++ Assets/Scripts/MenuPrincipal.cs
@@ -12,7 +12,7 @@ public class MenuPrincipal : MonoBehaviour
 
     public void Iniciar()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(1);
     }
 
     public void AbrirOpciones()
